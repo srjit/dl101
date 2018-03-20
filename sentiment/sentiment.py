54cb50eb3524239cfabc2e2711a7e1d07c4af972
Sreejith Sreekumar <sreekumar.s@MOBC02TQ1DTFVH4.tld>
2018-03-20 00:13:40 -0400

    sentiment model lstm

--- sentiment/sentiment.py
+++ sentiment/sentiment.py
@@ -8,22 +8,68 @@ wordsList = [word.decode('UTF-8') for word in wordsList] #Encode words as UTF-8
 wordVectors = np.load('wordVectors.npy')
 
 
-import tensorflow as tf
-maxSeqLength = 10
 
-firstSentence = np.zeros((maxSeqLength), dtype='int32')
-firstSentence[0] = wordsList.index("i")
-firstSentence[1] = wordsList.index("thought")
-firstSentence[2] = wordsList.index("the")
-firstSentence[3] = wordsList.index("movie")
-firstSentence[4] = wordsList.index("was")
-firstSentence[5] = wordsList.index("incredible")
-firstSentence[6] = wordsList.index("and")
-firstSentence[7] = wordsList.index("inspiring")
+from os import listdir
+from os.path import isfile, join
+positiveFiles = ['positiveReviews/' + f for f in listdir('positiveReviews/') if isfile(join('positiveReviews/', f))]
+negativeFiles = ['negativeReviews/' + f for f in listdir('negativeReviews/') if isfile(join('negativeReviews/', f))]
+numWords = []
+for pf in positiveFiles:
+    with open(pf, "r") as f:
+        line=f.readline()
+        counter = len(line.split())
+        numWords.append(counter)       
+print('Positive files finished')
 
-with tf.Session() as sess:
-    mat = tf.nn.embedding_lookup(wordVectors, firstSentence).eval()
+for nf in negativeFiles:
+    with open(nf, "r") as f:
+        line=f.readline()
+        counter = len(line.split())
+        numWords.append(counter)  
+print('Negative files finished')
 
+numFiles = len(numWords)
+print('The total number of files is', numFiles)
+print('The total number of words in the files is', sum(numWords))
+print('The average number of words in the files is', sum(numWords)/len(numWords))
+
+import re
+strip_special_chars = re.compile("[^A-Za-z0-9 ]+")
+
+def cleanSentences(string):
+    string = string.lower().replace("<br />", " ")
+    return re.sub(strip_special_chars, "", string.lower())
+
+
+# loading vectorized and saved file
+ids = np.load('idsMatrix.npy')
+
+from random import randint
+
+def getTrainBatch():
+    labels = []
+    arr = np.zeros([batchSize, maxSeqLength])
+    for i in range(batchSize):
+        if (i % 2 == 0): 
+            num = randint(1,11499)
+            labels.append([1,0])
+        else:
+            num = randint(13499,24999)
+            labels.append([0,1])
+        arr[i] = ids[num-1:num]
+    return arr, labels
+
+def getTestBatch():
+    labels = []
+    arr = np.zeros([batchSize, maxSeqLength])
+    for i in range(batchSize):
+        num = randint(11499,13499)
+        if (num <= 12499):
+            labels.append([1,0])
+        else:
+            labels.append([0,1])
+        arr[i] = ids[num-1:num]
+    return arr, labels
 
 
 maxSeqLength = 250
@@ -31,6 +77,29 @@ batchSize = 24
 lstmUnits = 64
 numClasses = 2
 iterations = 100000
+
+import tensorflow as tf
+tf.reset_default_graph()
+
+labels = tf.placeholder(tf.float32, [batchSize, numClasses])
+input_data = tf.placeholder(tf.int32, [batchSize, maxSeqLength])
+
+data = tf.Variable(tf.zeros([batchSize, maxSeqLength, numDimensions]),dtype=tf.float32)
+data = tf.nn.embedding_lookup(wordVectors,input_data)
+
+lstmCell = tf.contrib.rnn.BasicLSTMCell(lstmUnits)
+lstmCell = tf.contrib.rnn.DropoutWrapper(cell=lstmCell, output_keep_prob=0.25)
+value, _ = tf.nn.dynamic_rnn(lstmCell, data, dtype=tf.float32)
+
+weight = tf.Variable(tf.truncated_normal([lstmUnits, numClasses]))
+bias = tf.Variable(tf.constant(0.1, shape=[numClasses]))
+value = tf.transpose(value, [1, 0, 2])
+last = tf.gather(value, int(value.get_shape()[0]) - 1)
+prediction = (tf.matmul(last, weight) + bias)
+
+correctPred = tf.equal(tf.argmax(prediction,1), tf.argmax(labels,1))
+accuracy = tf.reduce_mean(tf.cast(correctPred, tf.float32))
+
 loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=labels))
 optimizer = tf.train.AdamOptimizer().minimize(loss)
 
@@ -40,13 +109,15 @@ tf.summary.scalar('Loss', loss)
 tf.summary.scalar('Accuracy', accuracy)
 merged = tf.summary.merge_all()
 logdir = "tensorboard/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S") + "/"
-writer = tf.summary.FileWriter(logdir, sess.graph)
+
+
 
 sess = tf.InteractiveSession()
 saver = tf.train.Saver()
 sess.run(tf.global_variables_initializer())
+writer = tf.summary.FileWriter(logdir, sess.graph)
 
-print "!!!"
+print "Running..."
 
 for i in range(iterations):
    #Next Batch of reviews
@@ -66,4 +137,18 @@ for i in range(iterations):
        print("saved to %s" % save_path)
 writer.close()
 
+
+
+## saving the model
+sess = tf.InteractiveSession()
+saver = tf.train.Saver()
+saver.restore(sess, tf.train.latest_checkpoint('models'))
+
+iterations = 10
+for i in range(iterations):
+    nextBatch, nextBatchLabels = getTestBatch();
+    print("Accuracy for this batch:", (sess.run(accuracy, {input_data: nextBatch, labels: nextBatchLabels})) * 100)
+
+
+    
 
